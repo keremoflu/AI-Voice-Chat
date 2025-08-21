12a249b6ff84d8c15f23284a8d7f794209cc943b
Kerem Oflu <keremoflu92@gmail.com>
2025-08-21 15:30:07 +0300

    first setup

---
+++ AIVoiceChat/AIVoiceChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIVoiceChatApp.swift
+//  AIVoiceChat
+//
+//  Created by Kerem on 21.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct AIVoiceChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
