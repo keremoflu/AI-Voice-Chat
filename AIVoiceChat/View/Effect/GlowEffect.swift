2afc60f0e9384de37dd85b3aa474d7321f4c46d8
Kerem Oflu <keremoflu92@gmail.com>
2025-08-23 16:41:05 +0300

    add glow effect

---
+++ AIVoiceChat/View/Effect/GlowEffect.swift
@@ -0,0 +1,57 @@
+//
+//  GlowEffect.swift
+//  AIVoiceChat
+//
+//  Created by Kerem on 23.08.2025.
+//
+
+import SwiftUI
+
+struct GlowEffect: View {
+    
+    @Binding var isAnimating: Bool
+    @State private var isGrowing = false
+    
+    @State private var lineWidth: CGFloat = 16.0
+    
+    var body: some View {
+        ZStack {
+            // Your main content
+            Color.clear
+                .ignoresSafeArea()
+
+            // Glow overlay
+            RoundedRectangle(cornerRadius: 40)
+                .strokeBorder(
+                    AngularGradient(
+                        gradient: Gradient(colors: [
+                            .primaryRed.opacity(0.8)
+                        ]),
+                        center: .center
+                    ),
+                    lineWidth: isGrowing ? 16 : 8
+                )
+                .blur(radius: 10)
+                .opacity(0.8)
+        }
+        .ignoresSafeArea()
+
+        .animation(
+            Animation.easeInOut(duration: 0.5)
+                   .repeatForever(autoreverses: true),
+                   value: isGrowing
+        )
+        .onChange(of: isAnimating) { newValue in
+            isGrowing = isAnimating
+        }
+        .onAppear {
+            isGrowing = true
+        }
+        
+    }
+}
+
+
+#Preview {
+    GlowEffect(isAnimating: .constant(true))
+}
