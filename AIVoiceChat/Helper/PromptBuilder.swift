00f9b4e52cec7cd0ee7e70adc24e7bfa90ade3f2
Kerem Oflu <keremoflu92@gmail.com>
2025-08-24 16:06:58 +0300

    update prompt

--- AIVoiceChat/Helper/PromptBuilder.swift
+++ AIVoiceChat/Helper/PromptBuilder.swift
@@ -12,10 +12,10 @@ struct PromptBuilder {
         
         if let lastUserMessage = UserDefaultsManager.shared.lastMessage {
             print("prompt: \("Please answer this question: \(message) Analyze language and response as same language type. Make responses short. (Give answer based on this last message (if related) : -\(lastUserMessage)-)")")
-             return "Please answer this question: \(message) Analyze language and response as same language type. Make responses short."
+             return "Please answer this question: \(message) Analyze language and response as same language type. Make responses short. Don't add special characters"
         } else {
             print("prompt else: \("Please answer this question: \(message) Analyze language and response as same language type. Make responses short.")")
-            return "Please answer this question: \(message) Analyze language and response as same language type. Make responses short."
+            return "Please answer this question: \(message) Analyze language and response as same language type. Make responses short. Don't add special characters"
         }
         
     }
